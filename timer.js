1f0961ffe1db743f6443a1fcb00b336bad9a4b01
Drake Champagne <trivorak@gmail.com>
2016-03-26 04:51:12 -0500

    inital kinda commit

---
+++ timer.js
@@ -0,0 +1,43 @@
+var totalSeconds = 0;
+
+//Set timer directly in seconds
+function setTimer(seconds){
+totalSeconds = parseInt(seconds);
+}
+
+//Converts time in minutes and seconds and then sets timers duration directly
+function convertToSeconds(min,sec){
+  let minutes = parseInt(min * 60);
+  totalSeconds = parseInt(min + sec);
+}
+
+//Converts time(in seconds) into a readable minutes:seconds ex. 2:35
+function getHumanReadableOutput(seconds){
+  let minuteValue = Math.floor(seconds / 60);
+  let secondValue = seconds % 60;
+  let humanReadableOutput = minuteValue + ":" + secondValue;
+  return humanReadableOutput;
+}
+
+//Startup Countdown Timer
+function startTimer(){
+  if (totalSeconds <= 0){
+    console.log("Yo Shit's Done!");
+  } else {
+    setTimeout(() =>{
+      decreaseTime();
+      logRemainingTime(totalSeconds);
+      startTimer();
+      },1000);
+  }
+}
+
+//Subtracts 1 from the total time at the interval of the timerLoop
+function decreaseTime(){
+  totalSeconds = totalSeconds - 1;
+}
+
+//function for console log display using the getHumanReadableOutput function for formatting
+function logRemainingTime(time){
+  console.log(getHumanReadableOutput(time));
+}
