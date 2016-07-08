7be5d28dbb2b5aa0a1b3064cba722c7074d2fd28
Zak Angelle <zaknotzak@gmail.com>
2016-07-08 02:29:54 -0500

    Remove pwd shit

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const app = express();
 
+const app = express();
 const port = process.env.PORT || 8080;
-process.env.PWD = process.cwd();
 
 app.use(express.static(process.env.PWD + '/dist'));
 
